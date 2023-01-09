7368bde0c7559794ab03fbb36fd71d31c809634c
end1996 <116982425+end1996@users.noreply.github.com>
2023-01-09 14:46:00 -0500

    Cambio en main y mant tipo usuario

--- lib/application/frm_mant_tipo_usuario.dart
+++ lib/application/frm_mant_tipo_usuario.dart
@@ -17,7 +17,7 @@ class _frm_mant_tipo_usuario extends State<frm_mant_tipo_usuario> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: const Text('Mantenimiento tipo usuario enviado a GIT'),),
+      appBar: AppBar(title: const Text('Mantenimiento tipo usuario enviado a GIT 2.0'),),
       body: Column(
         children: [
           TextField(
